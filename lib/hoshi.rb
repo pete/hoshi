2db642365433f2f88df4bf6463d0a2eae212dff9
Pete Elmore <1337p337@gmail.com>
2008-11-05 21:55:23 -0800

    Documenting...

--- lib/hoshi.rb
+++ lib/hoshi.rb
@@ -4,7 +4,9 @@ require 'hoshi/monkey_patches'
 require 'hoshi/tag'
 require 'hoshi/view'
 
-# This is the 
+# This is the namespace for all of Hoshi, which currently only includes
+# Hoshi::View and Hoshi::Tag .  For an overview, see doc/README .  For
+# specifics, check out Hoshi::View .
 module Hoshi
 	# This is a cosmetic method; you may do Hoshi::View[:type],
 	# Hoshi::View(:type), or Hoshi::View :type
--- lib/hoshi/tag.rb
+++ lib/hoshi/tag.rb
@@ -1,24 +1,39 @@
 module Hoshi
-	# Represents an HTML tag.
+	# Represents an HTML tag.  You usually won't be using this class directly.
 	class Tag
 		attr_accessor :name, :close_type
 
+		# A tag currently has only two attributes:  a name and a method for
+		# closing it, which both decide how it is rendered as a string. 
+		# A self-closing tag:
+		#   Tag.new('test', :self).render # => "<test />"
+		# A tag that does not need to close:
+		#   Tag.new('test', :none).render('test this') # => "<test>test this\n"
+		# And a regular tag:
+		#   Tag.new('test').render # => "<test></test>"
 		def initialize(name, close_type = nil)
 			@name, @close_type = name, close_type
 		end
 
+		# Generates a string from this tag.  inside should be the contents to
+		# put between the opening and closing tags (if any), and opts are the
+		# HTML options to put in the tag.  For example,
+		#   Tag.new('div').render('Click for an alert.', 
+		#                         :onclick => "alert('Hi.');")
+		# gets you this:
+		#   <div onclick="alert('Hi.');">Click for an alert.</div>
 		def render(inside = nil, opts = {})
 			inside = inside.to_s
 
 			s = "<#{name} #{opts.to_html_options}".strip
 			if inside.empty? && close_type == :self
-				return s << "/>"
+				return s << " />"
 			end
 
 			s << ">" << inside
 
 			if close_type == :none
-				s
+				s << "\n"
 			else
 				s << "</#{name}>"
 			end
