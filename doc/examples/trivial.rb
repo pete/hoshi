1604c48eb4a7fb1a9713d90ced671d0cbddbcf7e
Pete Elmore <1337p337@gmail.com>
2008-11-08 18:35:26 -0800

    %!#!$#@ vim doesn't do blocks right.

--- doc/examples/trivial.rb
+++ doc/examples/trivial.rb
@@ -5,14 +5,14 @@ class Trivial < Hoshi::View :html4
 		doctype
 		html {
 			head {
-			title "Hello, world!"
-			link :rel => 'stylesheet', :href => '/css/hoshi.css'
-		}
+				title "Hello, world!"
+				link :rel => 'stylesheet', :href => '/css/hoshi.css'
+			}
 
-		body {
-			h1 "Hello, world!"
-			p "This is a greeting to the world."
-		}
+			body {
+				h1 "Hello, world!"
+				p "This is a greeting to the world."
+			}
 		}
 		render
 	end
