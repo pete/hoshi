899ed28d924482e76e89f6cc465bff890c47234a
Pete Elmore <1337p337@gmail.com>
2008-11-11 00:33:04 -0800

    html2hoshi now generates nicer code, uses hpricot, parses xhtml/xml better, generates correct ruby code (in the case of tags named things that you can't call methods in ruby), and also is now so huge it's an abomination.
    Also, update to the Rakefile and TODO.

--- lib/html2hoshi.rb
+++ lib/html2hoshi.rb
@@ -1,4 +1,4 @@
-require 'rubyful_soup'
+require 'hpricot'
 
 module Hoshi
 	# A semi-hacky method for converting HTML text to a Hoshi method.  It takes
@@ -11,35 +11,75 @@ module Hoshi
 
 	private
 
-	def self.parse(html)
-		BeautifulSoup.new(html)
+	def self.parse(doc)
+		Hpricot.method(
+			if doc.include?('<?xml') # Seems like a safe assumption.
+				:XML
+			else
+				:parse
+			end).call(doc)
 	end
 
 	def self.tree_to_hoshi(tree, indent = "\t", indentlevel = 0)
 		idt = indent * indentlevel
-		(tree.to_a rescue []).inject('') { |str, i|
-			next(str) if i == "\n"
-			str << "\n#{idt}" << 
-			if i.kind_of? ::Tag # RubyfulSoup:  namespaceless.  orz
-				s = i.name.dup
+		str = ''
+		tree.each_child { |i|
+			str << ("\n#{idt}" <<
+			case i
+			when Hpricot::Elem
+				# Something simple that resembles rules for ruby method naming.
+				# I know that both XML and Ruby allow most valid unicode 
+				# characters in tags/method names, but this is Good Enough.
+				using_tag = !/^[a-z][a-zA-Z0-9_]*[!?]?$/.match(i.name)
 
-				unless i.attrs.empty?
-					attrs = i.attrs.dup
-					# Workaround for an apparent RubyfulSoup bug:
-					attrs.delete('img') if i.name == 'img'
-					s << '(' << 
-						attrs.inspect.sub(/^\{/, '').sub(/\}$/, '') << ')'
+				args, block = [], nil
+				s = if using_tag
+						args << i.name.inspect << 'nil'
+						'tag'
+					else
+						i.name
+					end
+
+				if i.children.size == 1 && 
+				   i.children.first.kind_of?(Hpricot::Text)
+					fc = i.children.first.to_s.strip
+					args << fc.inspect unless fc.empty?
+				elsif !i.children.empty?
+					block = ' {' << 
+						tree_to_hoshi(i, indent, indentlevel + 1) <<
+						"\n#{idt}}"
 				end
 
-				inner = tree_to_hoshi i, indent, indentlevel + 1
-				if inner.empty?
-					s
+				unless i.attributes.empty?
+					args << i.attributes.inspect.sub(/^\{/, '').sub(/\}$/, '')
+				end
+
+				unless args.empty?
+					arg_s = args.join(', ')
+					if block
+						s << "(#{arg_s})"
+					else
+						s << " #{arg_s}"
+					end
+				end
+
+				s << block if block
+
+				s
+			when Hpricot::Comment
+				"comment #{i.content.inspect}"
+			when Hpricot::BogusETag
+				"raw #{i.to_s.inspect} # Dangling end tag."
+			when Hpricot::DocType, Hpricot::ProcIns, Hpricot::XMLDecl, 
+			     Hpricot::Text, Object
+				x = i.to_s
+				if /\A\s*\Z/.match(x)
+					next
 				else
-					s << " {#{inner}\n#{idt}}"
+					"raw #{i.to_s.strip.inspect}"
 				end
-			else
-				"raw #{i.to_s.inspect}"
-			end
+			end)
 		}
+		str
 	end
 end
