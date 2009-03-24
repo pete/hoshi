15053c101a3ed9e436d85957dddaa7a609b1b628
Pete Elmore <1337p337@gmail.com>
2009-03-24 16:00:08 -0700

    whitespace changes for the win amirite guys

--- lib/hoshi/view/html4.rb
+++ lib/hoshi/view/html4.rb
@@ -5,14 +5,13 @@ class Hoshi::View
 		dtd! "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" " \
 			"\"http://www.w3.org/TR/html4/strict.dtd\">"
 
-
 		tags *%w(a address applet area b base basefont bdo blockquote body
 				 button caption center col colgroup dd div dl dt fieldset font
 				 form frame frameset h1 h2 h3 h4 h5 h6 head hr html iframe img
 				 input isindex label legend li link map meta noframes noscript
 				 object ol optgroup option p param pre q script select span
 				 style table tbody textarea tfoot thead title tr td ul)
-		 open_tags *%w(br)
+		open_tags *%w(br)
 	end
 end
 
