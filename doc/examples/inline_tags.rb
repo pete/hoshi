bc4e6c112b8b1fcca57255de2434d8b100b8c8a6
Pete Elmore <1337p337@gmail.com>
2009-05-12 19:08:08 -0700

    Adding the inline tags example.  Should at some point bring all of the examples up to date.

---
+++ doc/examples/inline_tags.rb
@@ -0,0 +1,21 @@
+require 'hoshi'
+
+puts(Hoshi::View(:html4) {
+	doc {
+		head {
+			title "Hello, world!"
+			link :rel => 'stylesheet', :href => '/css/hoshi.css'
+		}
+
+		body {
+			h1 "Hello, world!"
+			p "This is a greeting to the world."
+			p { 
+				"And #{_a 'this', :href => '/'} is an inline tag, prefixed "\
+					"by '_'."
+			}
+
+		}
+	}
+	render
+})
