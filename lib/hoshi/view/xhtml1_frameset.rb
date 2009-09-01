27a5e4d12f8c500519099ddf9d6b281e2907bc88
Pete Elmore <1337p337@gmail.com>
2009-08-31 19:24:36 -0700

    Reformat that.

--- lib/hoshi/view/xhtml1_frameset.rb
+++ lib/hoshi/view/xhtml1_frameset.rb
@@ -2,8 +2,8 @@ require 'hoshi/view/xhtml1'
 
 class Hoshi::View
 	class XHTML1Frameset < XHTML1
-		dtd! "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 " \
-			"Frameset//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">"
+		dtd! "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Frameset//EN\" " \
+			"\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\">"
 		tags *%w(a abbr acronym address applet area b base basefont bdo big
 				 blockquote body br button caption center cite code col
 				 colgroup dd del dfn dir div dl dt em fieldset font form frame
