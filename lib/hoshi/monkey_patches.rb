aa2c3154b524ec43250d4f643329cab62c868bf2
Pete Elmore <pete@debu.gs>
2013-11-25 13:50:27 -0800

    1.9 has Symbol#to_proc, so this is needless.

--- lib/hoshi/monkey_patches.rb
+++ lib/hoshi/monkey_patches.rb
@@ -1,24 +1,17 @@
+# TODO:  When 1.9 goes away, monkey-patching should be changed to refinements.
+# See: http://blog.headius.com/2012/11/refining-ruby.html
+
 class Hash
 	# Makes this hash fit to be put into a tag.
-	# { :a => 1, :b => "two", :c => true }.to_html_options # => 'a="one" b="two" c'
+	# {a: 1, b: "two", c: true}.to_html_options # => 'a="one" b="two" c'
 	def to_html_options double_quotes = true
 		qchar = double_quotes ? '"' : "'"
 		map { |k,v|
-			if v == true
+			if v == true # Intentional.
 				k.to_s
 			else
 				"#{k}=#{qchar}#{v}#{qchar}"
 			end
 		}.join(' ')
-	end
-end
-
-# We've dropped facets, and are now shooting for compatibility across Ruby
-# versions, so we add Symbol#to_proc unless it exists.
-unless((Symbol.instance_method(:to_proc) rescue nil))
-	class Symbol
-		def to_proc
-			proc { |obj, *args| obj.send(self, *args) }
-		end
 	end
 end
