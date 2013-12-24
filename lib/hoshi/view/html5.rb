e01de0e222678a271aacb0d4e388724d1563c627
Pete Elmore <pete@debu.gs>
2013-12-23 23:58:07 -0800

    I don't know how people keep track of this stuff.

--- lib/hoshi/view/html5.rb
+++ lib/hoshi/view/html5.rb
@@ -5,19 +5,20 @@ class Hoshi::View
 		dtd! "<!DOCTYPE html>"
 
 		tags *%w(
-			a abbr address area article aside audio b base bb bdi
-			bdo blockquote body button canvas caption cite code col
-			colgroup command data datagrid datalist dd del details
-			dfn div dl dt em embed eventsource fieldset figcaption
-			figure footer form h1 h2 h3 h4 h5 h6 head header hgroup
-			hr html i iframe img input ins kbd keygen label legend
-			li link mark map menu meta meter nav noscript object ol
-			optgroup option output p param pre progress q ruby rp rt
-			s samp script section select small source span strong
-			style sub summary sup table tbody td textarea tfoot th
-			thead time title tr track u ul var video wbr
+			a abbr address area article aside audio b base bb
+			bdi bdo blockquote body button canvas caption cite
+			code col colgroup command data datagrid datalist
+			dd del details dfn div dl dt em embed eventsource
+			fieldset figcaption figure footer form h1 h2 h3 h4
+			h5 h6 head header hgroup html i iframe img ins kbd
+			keygen label legend li mark map menu meta meter
+			nav noscript object ol optgroup option output p
+			param pre progress q ruby rp rt s samp script
+			section select small source span strong style
+			sub summary sup table tbody td textarea tfoot
+			th thead time title tr track u ul var video wbr
 		)
-		open_tags *%w(br)
+		open_tags *%w(br hr link input)
 	end
 end
 
