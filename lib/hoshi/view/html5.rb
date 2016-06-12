fa58f72a150394742c745f0cc189e6c2176015da
Pete Elmore <pete@debu.gs>
2016-06-12 04:48:27 -0700

    Tag tweaks.

--- lib/hoshi/view/html5.rb
+++ lib/hoshi/view/html5.rb
@@ -5,21 +5,22 @@ class Hoshi::View
 		dtd! "<!DOCTYPE html>"
 
 		tags *%w(
-			a abbr address area article aside audio b base bb
-			bdi bdo blockquote body button canvas caption cite
-			code col colgroup command data datagrid datalist
-			dd del details dfn div dl dt em embed eventsource
-			fieldset figcaption figure footer form h1 h2 h3 h4
-			h5 h6 head header hgroup html i iframe ins kbd
-			keygen label legend li mark map menu meta meter
-			nav noscript object ol optgroup option output p
-			param pre progress q ruby rp rt s samp script
-			section select small source span strong style
-			sub summary sup table tbody td textarea tfoot
-			th thead time title tr track u ul var video wbr
+			a abbr address area article aside audio b base
+			bb bdi bdo blockquote body button canvas caption
+			cite code col colgroup command data datagrid
+			datalist dd del details dfn div dl dt em embed
+			eventsource fieldset figcaption figure footer
+			form h1 h2 h3 h4 h5 h6 head header hgroup html i
+			iframe ins kbd keygen label legend li mark map
+			menu meter nav noscript object ol optgroup
+			option output p param pre progress q ruby rp rt
+			s samp script section select small source span
+			strong style sub summary sup table tbody td
+			textarea tfoot th thead time title tr track u ul
+			var video wbr
 		)
 		open_tags *%w(
-			br hr img input link
+			br hr img input link meta
 		)
 	end
 end
