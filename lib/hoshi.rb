d909131c8a8007c38310ee4a40172aea23a5e2f0
Pete Elmore <pete@debu.gs>
2017-06-27 13:27:38 -0700

    doc cleanup

--- lib/hoshi.rb
+++ lib/hoshi.rb
@@ -4,7 +4,7 @@ require 'hoshi/tag'
 require 'hoshi/view'
 
 # This is the namespace for all of Hoshi, which currently only includes
-# Hoshi::View and Hoshi::Tag .  For an overview, see doc/README .  For
+# Hoshi::View and Hoshi::Tag .  For an overview, see the README.  For
 # specifics, check out Hoshi::View .
 module Hoshi
 	# This is a cosmetic method; you may do Hoshi::View[:type],
